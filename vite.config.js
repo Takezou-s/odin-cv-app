bbe6a7e31476f87a17c638561978ab89358b43c8
Takezou0 <bkaya1228@gmail.com>
2023-09-26 17:36:26 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import autoprefixer from "autoprefixer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/odin-cv-app/",
   css: {
     postcss: {
       plugins: [autoprefixer()],
